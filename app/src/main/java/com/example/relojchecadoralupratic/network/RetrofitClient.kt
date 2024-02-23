348dc64c0a05d0e8670737af9c60729456ce4503
AJfreeblack00110000 <143929391+unpseudonimo@users.noreply.github.com>
2024-02-23 14:55:14 -0600

    retro

--- app/src/main/java/com/example/relojchecadoralupratic/network/RetrofitClient.kt
+++ app/src/main/java/com/example/relojchecadoralupratic/network/RetrofitClient.kt
@@ -1,47 +1,18 @@
-import com.example.relojchecadoralupratic.network.ApiService
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+package com.example.relojchecadoralupratic.network
+
+import com.example.relojchecadoralupratic.core.Constantes
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 object RetrofitClient {
-    private const val BASE_URL = "https://tu.url.base/aqui/"
-
-    // Configurar un TrustManager personalizado para aceptar todos los certificados
-    private val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-
-        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-
-        override fun getAcceptedIssuers(): Array<X509Certificate> {
-            return arrayOf()
-        }
-    })
-
-    // Crear un cliente OkHttp con la configuración de TrustManager personalizado
-    private val okHttpClient = OkHttpClient.Builder()
-        .sslSocketFactory(
-            SSLContext.getInstance("SSL").apply {
-                init(null, trustAllCerts, SecureRandom())
-            }.socketFactory,
-            trustAllCerts[0] as X509TrustManager
-        )
-        .hostnameVerifier { _, _ -> true }
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)) // Agregar un interceptor para el registro de solicitudes y respuestas
-        .build()
 
     // Propiedad webService utilizando la delegación by lazy para inicialización perezosa
     val webService: ApiService by lazy {
         // Crear una instancia de Retrofit con la URL base y un convertidor Gson
         Retrofit.Builder()
-            .baseUrl(BASE_URL) // URL base del servicio web
-            .client(okHttpClient) // Usar el cliente OkHttp personalizado
-            .addConverterFactory(GsonConverterFactory.create()) // Convertidor Gson
+            .baseUrl(Constantes.API_URL) // URL base del servicio web
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create())) // Convertidor Gson
             .build()
             .create(ApiService::class.java) // Crear la instancia de WebService a partir de la interfaz
     }
